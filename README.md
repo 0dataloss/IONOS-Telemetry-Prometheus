322b58dba6950bb25c3cd054f8a7135781b89788
0dataloss <github@t-hoster.com>
2022-08-12 22:31:40 +0100

    Comments and Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # IONOS-Telemetry-Prometheus
-Python script made to convert the JSON format of the IONOS Telemetry API in a format which can be ingested by Promehteus
+This Python script has been written with the scope of transforming the information
+from the IONOS Telemetry API which are in JSON, to the Prometheus Ingestion Format.
+
+The IONOS Telemetry API is compatible with Grafana, but exposes only 14 days worth
+of metrics.
+With this script you will be able to record all the metric in a Prometheus server 
+of your choice and keep metrics for as long as you like
+
+How to run it:
+To Be Completed
--- Telemetry2Prometheus.py
+++ Telemetry2Prometheus.py
@@ -24,9 +24,13 @@ def config():
   return token
 
 def get_catalog(token):
+    # Build Auth Headers
     autHead={'Authorization': 'Bearer ' + token}
+    # Set URL for request
     telemetryUrl="https://api.ionos.com/telemetry/api/v1/series"
+    # Execute Request
     seriesResp = requests.get(telemetryUrl, headers=autHead)
+    # Extract Data and build list of unique values
     seriesRespJson=(seriesResp.json())
     extractData=seriesRespJson['data']
     singledata=[]
@@ -36,24 +40,35 @@ def get_catalog(token):
             continue
         else:
           singledata.append(value)
+    # Return List
     return singledata
 
 def retrieveSeries(catalog,token):
-    prometheusPagePrintadd="# This script is interrogating IONOS Telemetry API"
+    # Prepare Prometheus Help Line
+    prometheusPagePrintadd="# This script is interrogating IONOS Telemetry API More Info to Come"
+    # Prepare Auth Headers
     autHead={'Authorization': 'Bearer ' + token}
+    # Send a query for each Item in List singleData
     for serie in catalog:
         telemetryUrl="https://api.ionos.com/telemetry/api/v1/query?query=" + serie
         seriesResp = requests.get(telemetryUrl, headers=autHead)
         seriesRespJson=seriesResp.json()
+        # Extract the data
         seriesRespJson=seriesRespJson['data']['result']
+        # Loop inside the list of Items (Servers and what not)
         for index in seriesRespJson:
           metricName=index['metric']['__name__']
           metricDc=index['metric']['dc']
           metricUuid=index['metric']['uuid']
           metricValue=index['value'][1]
+          # Prepare the string Prometheus Likes
           printValue=metricName + "{DCName=\""+metricDc+"\",ResourceUUID=\""+metricUuid+"\"}"+ str(metricValue)
+          # Arrange all the string together
           prometheusPagePrintadd=prometheusPagePrintadd +"\n" + printValue
+    # Print all the strings as a single page
     return prometheusPagePrintadd
+
+# Define and star the Flask server on port 5000
 
 @app.route('/metrics')
 def main():
